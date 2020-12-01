26a1092e768def6aa6f4f68f7576323d8129a654
Daniel Wilson <daniel@loylap.com>
2020-12-01 14:24:57 +0000

    Updated dependencies and url for Guilherme testing.

--- app/src/main/java/com/myapp/MainActivity.kt
+++ app/src/main/java/com/myapp/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.myapp
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
 import android.webkit.WebView
 import android.widget.FrameLayout
@@ -7,16 +9,28 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    companion object{
+        private const val GUILHERMES_URL = "https://dev-dashboard.loylap.com/?version=0.1924.9638#/sign-up/form"
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_webview)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WebView.setWebContentsDebuggingEnabled(true)
+        }
 
         val webView = WebView(this)
         val container = findViewById<FrameLayout>(R.id.web_view_container)
         container.addView(webView)
 
         webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowFileAccessFromFileURLs = true
+        webView.settings.allowUniversalAccessFromFileURLs = true
 
-        webView.loadUrl("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select")
+        webView.loadUrl(GUILHERMES_URL)
     }
 }
